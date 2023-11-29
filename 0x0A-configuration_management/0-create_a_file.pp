d0ca72ffe8b65344d36ec3b0e75f5516d31f10ed
Beethoven Kasompe <kasbeet@gmail.com>
2023-11-29 15:58:51 -0800

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
-# creates a file in /tmp
+# Puppet script is used for createing a file in /tmp/school
 
 file { '/tmp/school':
+  ensure  => file,
   content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
