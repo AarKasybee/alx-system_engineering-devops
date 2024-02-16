77058c95716d40b8273ac536349354045bbad380
Beethoven <kasbeet@gmail.com>
2024-02-16 04:04:35 -0800

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,15 +4,20 @@
 import requests
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    Return the total number of subscribers on a given subreddit.
 
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The total number of subscribers.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         'User-Agent': 'My User Agent 1.0'
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
     if response.status_code == 200:
         data = response.json()
         return data.get('data').get('subscribers')
