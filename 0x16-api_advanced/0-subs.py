59672f10298a25a01f60242c08953bd27c61027b
Beethoven <kasbeet@gmail.com>
2024-02-16 04:14:49 -0800

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -23,6 +23,8 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         data = response.json()
         return data.get('data').get('subscribers')
+    elif response.status_code == 404:
+        return 0
     else:
         return 0
 
