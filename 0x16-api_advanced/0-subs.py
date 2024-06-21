95277a93b01e97b31c9db28c723fadb84e343d6b
Beethoven <kasbeet@gmail.com>
2024-06-20 17:54:27 -0700

    try2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API and returns the number of subscribers"""
+"""function that queries Reddit API and returns the number of subscribers"""
 import requests
 
 
@@ -7,18 +7,10 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        'User-Agent': "linux:0x16.api.advanced:v1 (by /u/muntuwanga)"
-    }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 404:
-            return 0
-        try:
-            results = response.json().get("data", {})
-            return results.get("subscribers", 0)
-        except ValueError:
-            print(f"Error decoding JSON for subreddit: {subreddit}")
-            return 0
-    except requests.RequestException as e:
-        print(f"Request error: {e}")
+            'User-Agent': "linux:0x16.api.advanced:v1 (by /u/OkContract481)"
+            }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
