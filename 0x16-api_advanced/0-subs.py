2a523338b0d56a5157fd1b843bd1b8fd67331777
AarKasybee <123067575+AarKasybee@users.noreply.github.com>
2024-06-21 02:13:42 +0200

    last update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,18 +2,15 @@
 """function that queries Reddit API and returns the number of subscribers"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        'User-Agent': "linux:0x16.api.advanced:v1 (by /u/muntuwanga)"
-    }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 404:
-            return 0
-        results = response.json().get("data", {})
-        return results.get("subscribers", 0)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+            'User-Agent': "linux:0x16.api.advanced:v1 (by /u/muntuwanga)"
+            }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
