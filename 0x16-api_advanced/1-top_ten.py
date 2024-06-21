6d289a08f99ba2d646eb093a967db094bded5b4d
Beethoven <kasbeet@gmail.com>
2024-06-20 18:15:15 -0700

    tpten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,37 +1,23 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints the titles of the
-first 10 hot posts listed for a given subreddit.
-"""
-
+"""function that queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Print the titles of the 10 hottest posts on a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to query.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/OkContract481)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "limit": 10
     }
-
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        if response.status_code == 200:
-            results = response.json().get("data", {}).get("children", [])
-            if results:
-                for post in results:
-                    print(post.get("data", {}).get("title"))
-            else:
-                print("None")
-        else:
-            print("None")
-    except requests.RequestException:
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
+
